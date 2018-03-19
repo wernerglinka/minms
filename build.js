2cb23ed8a9e491afb095c0af4b83232ca3ba7850
werner glinka <werner@bussedesign.com>
2018-03-19 13:46:00 -0500

    removed all unnecessary code

--- build.js
+++ build.js
@@ -1,15 +1,12 @@
 const Metalsmith = require('metalsmith');
 const inPlace = require('metalsmith-in-place');
-const path = require('path');
 
 const metalsmith = Metalsmith(__dirname)
   .source('dev/content')
   .destination('build')
-  //.ignore(__dirname + "/**/_*.njk")
   .clean(true);
 
 metalsmith.use(inPlace({
-  "pattern": "**/*.njk",
   "engineOptions": {
     root: __dirname + '/dev/'
   }
