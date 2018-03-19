f81f62fb6b449158a8c6930fda0b55b28fcaca5c
werner glinka <werner@bussedesign.com>
2018-03-19 14:07:01 -0500

    added a filter to junjucks env

--- build.js
+++ build.js
@@ -1,5 +1,17 @@
 const Metalsmith = require('metalsmith');
 const inPlace = require('metalsmith-in-place');
+const dateFilter = require("nunjucks-date-filter");
+
+const UTCdate = function (date) {
+    "use strict";
+    return date.toUTCString();
+}
+
+
+
+
+
+
 
 const metalsmith = Metalsmith(__dirname)
   .source('dev/content')
@@ -8,7 +20,11 @@ const metalsmith = Metalsmith(__dirname)
 
 metalsmith.use(inPlace({
   "engineOptions": {
-    root: __dirname + '/dev/'
+    root: __dirname + '/dev/',
+    filters: {
+        dateFilter: dateFilter,
+        UTCdate: UTCdate
+    }
   }
 }))
 
